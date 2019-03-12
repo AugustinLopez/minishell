e435d1c315c4f4307029dc001688c1aa2cf40eb4
Augustin LOPEZ <aulopez@e3r8p20.42.fr>
2019-03-12 19:10:56 +0100

    reduce size of code, need to correct parsing

--- srcs/ms_bonus_quote.c
+++ srcs/ms_bonus_quote.c
@@ -6,89 +6,99 @@
 /*   By: aulopez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 17:17:30 by aulopez           #+#    #+#             */
-/*   Updated: 2019/03/12 16:56:54 by aulopez          ###   ########.fr       */
+/*   Updated: 2019/03/12 19:10:38 by aulopez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <minishell.h>
 
-int			read_if_necessary(t_minishell *ms)
+static int			do_i_need_to_read(char **input, int previous)
 {
-	size_t	i;
-	int		j;
-	int		quote;
-	int		dquote;
-	int		backsl;
-	char	*tmp;
-	char	*tmp2;
+	int option;
+	size_t i;
 
-	if (!ms->input)
-		return (0);
+	option = (previous == 3) ? 0 : previous;
 	i = 0;
-	quote = 0;
-	dquote = 0;
-	backsl = 0;
-	while ((ms->input)[i])
+	while ((*input)[i])
 	{
-		if ((ms->input)[i] == '\\' && (ms->input)[i + 1])
+		if ((*input)[i] == '\\' && (*input)[i + 1])
 		{
-			i+= 2;
+			i += 2;
 			continue ;
 		}
-		if (!dquote && !backsl && (ms->input)[i] == '\'')
-			quote = (!quote) ? 1 : 0;
-		if (!quote && !backsl && (ms->input)[i] == '\"')
-			dquote = (!dquote) ? 1 : 0;
-		if (!quote && !dquote && (ms->input)[i] == '\\' && !((ms->input)[i + 1]))
-			backsl = 1;
+		if ((!option || option == 1) && (*input)[i] == '\'')
+			option = !option ? 1 : 0;
+		if ((!option || option == 2) && (*input)[i] == '\"')
+			option = !option ? 2 : 0;
+		if (!option && (*input)[i] == '\\' && !((*input)[i + 1]))
+		{
+			(*input)[i] = 0;
+			return (3);
+		}
 		i++;
 	}
-	backsl ? ms->input[i - 1] = '\0' : 0;
-	while (dquote || quote || backsl)
+	return (option);
+}
+
+static int			read_again(t_minishell *ms, char **tmp, int option)
+{
+	int		j;
+
+	option == 1 ? ft_printf("quote> ") : 0;
+	option == 2 ? ft_printf("dquote> ") : 0;
+	option == 3 ? ft_printf("> ") : 0;
+	j = ft_gnl(0, tmp);
+	if (j == -1)
 	{
-		quote ? ft_printf("quote> ") : 0;
-		dquote ? ft_printf("dquote> ") : 0;
-		backsl ? ft_printf("> ") : 0;
-		j = ft_gnl(0, &tmp);
-		if (j == -1)
+		ft_dprintf(2, "Error: could not read stdin.\n");
+		ms_exit(ms, EXIT_FAILURE);
+	}
+	if (!j)
+	{
+		write(1, "\n", 1);
+		ms_exit(ms, 0);
+	}
+	return (1);
+}
+//> echo \
+//> "
+//dquote> "\
+//>
+//corriger ca
+int					read_if_needed(t_minishell *ms)
+{
+	char	*tmp;
+	char	*tmp2;
+	int		option;
+
+	option = do_i_need_to_read(&(ms->input), 0);
+	tmp = 0;
+	tmp2 = 0;
+	while (option && read_again(ms, &tmp, option))
+	{
+		if ((option == 3) && tmp[0] == '\\' && ft_iswhitespace(tmp + 1, 1))
 		{
-			ft_dprintf(2, "Error: could not read stdin.\n");
-			ms_exit(ms, EXIT_FAILURE);
-		}
-		if (ft_iswhitespace(tmp, 1) && backsl)
-		{
-			j == 1 && tmp ? free(tmp) : 0;
-			j == 0 ? write(1, "\n", 1) : 0;
-			if (!j)
-				ms_exit(ms, 0);
+			free(tmp);
 			continue ;
 		}
-		tmp2 = !backsl ? ft_sprintf("%s\n%s", ms->input, tmp) :
+		ft_printf("%d.%s\n",option, tmp);
+		tmp2 = (option != 3) ? ft_sprintf("%s\n%s", ms->input, tmp) :
 			ft_strjoin(ms->input, tmp);
-		ms->input ? free(ms->input) : 0;
+		option = do_i_need_to_read(&tmp, option);
+		tmp ? ft_memdel((void**)&tmp) : 0;
+		ms->input ? ft_memdel((void**)&(ms->input)) : 0;
+		if (!tmp2)
+			return (ms_error(-1, "Error: not enough memory.\n"));
 		ms->input = tmp2;
-		backsl = 0;
-		i = 0;
-		while (tmp[i])
-		{
-			if (tmp[i] == '\\' && tmp[i + 1])
-			{
-				i+= 2;
-				continue ;
-			}
-			if (!dquote && !backsl && tmp[i] == '\'')
-				quote = (!quote) ? 1 : 0;
-			if (!quote && !backsl && tmp[i] == '\"')
-				dquote = (!dquote) ? 1 : 0;
-			if (!quote && !dquote && tmp[i] == '\\' && !(tmp[i + 1]))
-				backsl = 1;
-			i++;
-		}
-		free(tmp);
 	}
 	return (0);
 }
+/*
+int				case_semicolon(t_minishell *ms, size_t (*ij)[2], t_list *last)
+{
+
+}*/
 
 int				split_ms(t_minishell *ms)
 {
@@ -96,14 +106,12 @@ int				split_ms(t_minishell *ms)
 	t_list	*last;
 	t_list	*tmp;
 	char	*zero;
-	int quote;
-	int dquote;
+	int option;
 	size_t	i;
 	size_t	j;
 	size_t	all;
 
-	quote = 0;
-	dquote = 0;
+	option = 0;
 	i = 0;
 	j = 0;
 	all = ft_strlen(ms->input);
@@ -112,11 +120,11 @@ int				split_ms(t_minishell *ms)
 	last = begin;
 	while ((ms->input[i]))
 	{
-		if (!quote && (ms->input)[i] == '\"')
-			dquote = (!dquote) ? 1 : 0;
-		else if (!dquote && (ms->input)[i] == '\'')
-			quote = (!quote) ? 1 : 0;
-		else if (!quote && !dquote && (ms->input)[i] == ';')
+		if ((!option || option == 1) && (ms->input)[i] == '\"')
+			option = (!option) ? 1 : 0;
+		else if ((!option || option == 2) && (ms->input)[i] == '\'')
+			option = (!option) ? 2 : 0;
+		else if (!option && (ms->input)[i] == ';')
 		{
 			if (!(i && ms->input[i - 1] == ' '))
 			{
@@ -138,14 +146,13 @@ int				split_ms(t_minishell *ms)
 			}
 			j = 0;
 		}
-		else if (!quote && !dquote && (ms->input)[i] == '\\')
+		else if (!option && (ms->input)[i] == '\\')
 		{
 			((char*)(last->pv))[j++] = (ms->input)[i++];
 			((char*)(last->pv))[j++] = (ms->input)[i++];
-
 			continue ;
 		}
-		if (!quote && !dquote && (ms->input)[i] == ' ')
+		if (!option && (ms->input)[i] == ' ')
 		{
 			if (last->zu == all + 2)
 				last->zu = 0;
@@ -181,15 +188,8 @@ int				split_ms(t_minishell *ms)
 
 void			ms_inputsplit(t_minishell *ms)
 {
-
-/*	char	*p;
-	
-	p = ft_strdup(ms->input);*/
-	read_if_necessary(ms);
+	if (read_if_needed(ms))
+		return ;
+		//return (-1);
 	split_ms(ms);
-	/*ft_printf("1.%s\nEnd.\n", ms->input);
-	free(ms->input);
-	ms->input = p;
-	(void)p;
-	(void)ms;*/
 }
